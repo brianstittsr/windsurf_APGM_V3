12c62b92ba005a40aaf187a33425d58c9584681b
brianstittsr <brianstittsr@gmail.com>
2026-02-26 22:58:28 -0500

    Remove 'Free consultation included • Touch-up included' from service selection page

--- src/app/book-now-custom/page.tsx
+++ src/app/book-now-custom/page.tsx
@@ -1031,17 +1031,6 @@ function BookNowCustomContent() {
           <p className="text-gray-600 text-lg md:text-xl max-w-2xl mx-auto">
             Select the permanent makeup service you&apos;d like to book
           </p>
-          <div className="mt-6 flex items-center justify-center gap-2 text-sm text-gray-500">
-            <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-            <span>Free consultation included</span>
-            <span className="mx-2">•</span>
-            <svg className="w-5 h-5 text-green-500" fill="currentColor" viewBox="0 0 20 20">
-              <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clipRule="evenodd" />
-            </svg>
-            <span>Touch-up included</span>
-          </div>
         </div>
           
         {servicesLoading && (
